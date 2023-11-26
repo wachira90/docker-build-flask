6148aa9675d81713d40b3b7cc225fe6acd750e2f
Wachira Duangdee <wcrcorp@gmail.com>
2023-11-27 00:21:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!python
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -5,10 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def root_path():
     return jsonify({'status': 'success'})
-  
+
 @app.route('/healthz')
 def health_check():
     return jsonify({'status': 'OK'})
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
